bcbec1f300567d6d8bcbd7be0f96aa847cd7863c
Gabriel <pucikplay@gmail.com>
2022-05-05 22:03:25 +0200

    Main and test swapped

--- app/main.c
+++ app/main.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
+#include <stddef.h>
+#include <unistd.h>
 #include <pthread.h>
-#include "stat_reader.h"
 
-int main() {
-    pthread reader;
+#include "stat_reader.h"
+#include "stat_analyzer.h"
+#include "stat_printer.h"
+#include "buffer_sync.h"
+
+int main()
+{
+    Buff_sync* reader_buffer = buff_sync_create(10);
+    Buff_sync* printer_buffer = buff_sync_create(10);
+
+    Analyzer_buffers* abuffs = abuffs_create(reader_buffer, printer_buffer);
+
+    pthread_t reader, analyzer, printer;
+    pthread_create(&reader, NULL, thread_read, (void*)&reader_buffer);
+    pthread_create(&analyzer, NULL, thread_analyze, (void*)&abuffs);
+    pthread_create(&printer, NULL, thread_print, (void*)&printer_buffer);
+
+    pthread_join(reader, NULL);
+    pthread_join(analyzer, NULL);
+    pthread_join(printer, NULL);
+
+    buff_sync_destroy(reader_buffer);
+    buff_sync_destroy(printer_buffer);
+
+    abuffs_destroy(abuffs);
 }
--- test/test.c
+++ test/test.c
@@ -5,20 +5,10 @@
 
 #include "stat_reader.h"
 #include "stat_analyzer.h"
+#include "stat_printer.h"
 #include "buffer_sync.h"
 
-int main(void) {
-
-    Buff_sync* bs = buff_sync_create(10);
-
-    pthread_t reader, analyzer;
-    pthread_create(&reader, NULL, thread_read, (void*)&bs);
-    pthread_create(&analyzer, NULL, thread_analyze, (void*)&bs);
-
-    pthread_join(reader, NULL);
-    pthread_join(analyzer, NULL);
-
-    buff_sync_destroy(bs);
-
-    return 0;
+int main(void)
+{
+    printf("This is a test prigram for CPU_tracker\n");
 }
