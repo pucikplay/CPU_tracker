9a01b6fd76df1bef94636239ab097f8cfc7e2a00
pucikplay <pucikplay@gmail.com>
2022-05-14 10:43:42 +0200

    Main extended for stopping threads on SIGTERM

--- app/main.c
+++ app/main.c
@@ -2,30 +2,52 @@
 #include <stddef.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <signal.h>
 
 #include "stat_reader.h"
 #include "stat_analyzer.h"
 #include "stat_printer.h"
 #include "buffer_sync.h"
+#include "stat_control.h"
+
+static Thread_stoppers* stop_controller;
+
+static void term_all_threads(int signum)
+{
+    if(signum == SIGTERM) {
+        tstop_stop_threads(stop_controller);
+    }
+}
 
 int main()
 {
-    Buff_sync* reader_buffer = buff_sync_create(10);
-    Buff_sync* printer_buffer = buff_sync_create(10);
+    Thread_checkers* work_controller = tcheck_create();
+    stop_controller = tstop_create();
 
-    Analyzer_buffers* abuffs = abuffs_create(reader_buffer, printer_buffer);
+    struct sigaction action = { 0, };
+    action.sa_handler = term_all_threads;
+    sigaction(SIGTERM, &action, NULL);
+
+    Buff_sync* reader_analyzer_buffer = buff_sync_create(10);
+    Buff_sync* analyzer_printer_buffer = buff_sync_create(10);
+
+    Reader_args* rargs = rargs_create(reader_analyzer_buffer);
+    Analyzer_args* aargs = aargs_create(reader_analyzer_buffer, analyzer_printer_buffer);
+    Printer_args* pargs = pargs_create(analyzer_printer_buffer);
 
     pthread_t reader, analyzer, printer;
-    pthread_create(&reader, NULL, thread_read, (void*)&reader_buffer);
-    pthread_create(&analyzer, NULL, thread_analyze, (void*)&abuffs);
-    pthread_create(&printer, NULL, thread_print, (void*)&printer_buffer);
+    pthread_create(&reader, NULL, thread_read, (void*)&rargs);
+    pthread_create(&analyzer, NULL, thread_analyze, (void*)&aargs);
+    pthread_create(&printer, NULL, thread_print, (void*)&pargs);
 
     pthread_join(reader, NULL);
     pthread_join(analyzer, NULL);
     pthread_join(printer, NULL);
 
-    buff_sync_destroy(reader_buffer);
-    buff_sync_destroy(printer_buffer);
+    aargs_destroy(aargs);
+    rargs_destroy(rargs);
+    pargs_destroy(pargs);
 
-    abuffs_destroy(abuffs);
+    tstop_destroy(stop_controller);
+    tcheck_destroy(work_controller);
 }
