32a9ac1784853bf065ea36ae63215cd8c9ae2be3
pucikplay <pucikplay@gmail.com>
2022-05-15 15:33:38 +0200

    analyzer_string_split now has more universal name: util_str_split

--- inc/stat_utils.h
+++ inc/stat_utils.h
@@ -4,7 +4,8 @@
 #include <stddef.h>
 #include <stdbool.h>
 
-char** analyzer_string_split(char* restrict data, const char delimiter, size_t* token_count);
+char** util_str_split(char* restrict data, const char delimiter, size_t* token_count);
+void util_split_cleanup(char** data_tokenized, size_t token_count);
 char* util_str_concat(char const* restrict str1, char const* restrict str2);
 bool reader_starts_with(const char *a, const char *b);
 
